dc97e966f80820a91537425d7c013f7ae9449cb8
Primus Mario Andaka Ginting <primusmag@gmail.com>
2026-01-13 13:56:48 +0700

    fix vulnerability

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,13 +1,7 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-} from '@angular/common/http';
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
 import { Observable, finalize } from 'rxjs';
 import { UiService } from '../services/ui.service';
-
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
   constructor(private ui: UiService) {}
